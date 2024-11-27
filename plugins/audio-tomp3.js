4146e28d9d32f34dc8f484eac46589d861e119ec
Dark-Robin <sihileld23@gmail.com>
2024-11-28 00:53:53 +0530

    Update audio-tomp3.js

--- plugins/audio-tomp3.js
+++ plugins/audio-tomp3.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
     if (!/video|audio/.test(mime)) throw `✳️ Reply to the video or voice note you want to convert to mp3 with the command :\n\n*${usedPrefix + command}*`*/
   let media = await q.download?.()
-  if (!media) throw '❎ Failed to download media'
+  if (!media) throw '❎ Failed to download media\n\n❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️'
   let audio = await toAudio(media, 'mp4')
-  if (!audio.data) throw '❎ Error converting'
+  if (!audio.data) throw '❎ Error converting\n\n❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️'
   conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
 }
 handler.help = ['tomp3']
