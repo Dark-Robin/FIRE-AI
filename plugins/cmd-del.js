1d2c94c3f0981fbce48584cc3f2c3c418e43ae7d
Dark-Robin <sihileld23@gmail.com>
2024-11-28 01:08:50 +0530

    Update cmd-del.js

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -3,11 +3,11 @@
 let handler = async (m, { text }) => {
   let hash = text
   if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-  if (!hash) throw `✳️ Enter the command name`
+  if (!hash) throw `✳️ Enter the command name\n\n❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️`
   let sticker = global.db.data.sticker
-  if (sticker[hash] && sticker[hash].locked) throw `✳️ You can not delete this command`
+  if (sticker[hash] && sticker[hash].locked) throw `✳️ You can not delete this command\n\n❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️`
   delete sticker[hash]
-  m.reply(`✅ Command eliminated`)
+  m.reply(`✅ Command deleted\n\n❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️`)
 }
 
 handler.help = ['cmd'].map(v => 'del' + v + ' <text>')
