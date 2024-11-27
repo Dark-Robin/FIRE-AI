9b5540eeae3e447f7193d72285b995e5c9903872
Dark-Robin <sihileld23@gmail.com>
2024-11-28 01:04:48 +0530

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
   let user = global.db.data.users[m.sender]
   let name = conn.getName(m.sender)
   let taguser = '@' + m.sender.split('@s.whatsapp.net')[0]
-  let av = `./Assets/mp3/${pickRandom(['firemd', 'firemd1'])}.mp3`
+  let av = `./Assets/mp3/${pickRandom(['Hiruu s', 'Sihilel'])}.mp3`
 
-  m.reply(`Hello ${taguser} FIRE MD🔥 IS AVAILABLE  type .menu `)
+  m.reply(`Hello ${taguser} ❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️ IS AVAILABLE  type .menu `)
   conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 }
 
